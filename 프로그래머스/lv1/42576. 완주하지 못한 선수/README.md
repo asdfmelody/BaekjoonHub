ab57ce064286dc8e476632d4a4d96b4a2a370189
asdfmelody <56292365+asdfmelody@users.noreply.github.com>
2023-01-13 04:21:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.33 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42576?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 25.4 MB, 시간: 77.09 ms
+메모리: 25.3 MB, 시간: 70.33 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -5,10 +5,18 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-	sort(participant.begin(), participant.end());
-	sort(completion.begin(), completion.end());
-	int i = 0;
-	for (; i < completion.size(); i++) 
-		if (participant[i] != completion[i]) break;
-	return participant[i];
+    string answer = "";
+    sort(participant.begin(),participant.end());
+    sort(completion.begin(),completion.end());
+    int len = completion.size();
+    bool found = 0;
+    for(int i=0;i<len;i++){
+        if(participant[i] != completion[i]){
+            answer = participant[i];
+            found = 1;
+            break;
+        }
+    }
+    if(found == 0) answer = participant[len];
+    return answer;
 }
