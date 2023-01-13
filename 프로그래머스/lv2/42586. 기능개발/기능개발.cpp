9ee2ab6249a4bb2aa530e42b68d0f19da485edd1
asdfmelody <56292365+asdfmelody@users.noreply.github.com>
2023-01-13 22:48:11 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -5,33 +5,31 @@
 
 using namespace std;
 
-int workday(int progress, int speed){
-    int ret = ceil((double)(100 - progress)/(double)speed);
-    return ret;
-}
-
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> left;
+    vector<int> bapo; // 배포 날짜
+    
     int len = progresses.size();
-    // left days
+    
     for(int i=0;i<len;i++){
-        left.push_back(workday(progresses[i],speeds[i]));
-        //cout<<left[i];
+        int left_progress = 100 - progresses[i];
+        int days = ceil((float)left_progress / (float)speeds[i]); // 올림해야됨.
+        bapo.push_back(days);
     }
-    //
-    int front = left[0];
-    int cnt=1;
+    
+    int d_day = bapo[0];
+    int cnt = 1;
     for(int i=1;i<len;i++){
-        if(front < left[i]) {
+        if(d_day >= bapo[i]) cnt++;
+        else{
             answer.push_back(cnt);
-            front = left[i];
+            d_day = bapo[i];
             cnt = 1;
         }
-        else {
-            cnt++;
-        }
     }
+    
     answer.push_back(cnt);
+    
+    
     return answer;
 }
